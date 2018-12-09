98b532811906e383f366e9a97aa95961588c9bde
igimia <ayaka@ayaka.local>
2018-12-09 16:10:04 +0900

    fix: babelVer, webpackVer Adjust

---
+++ volumes/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  devtool: 'inline-source-map', // ソースマップファイル追加
+  mode: 'development',
+  entry: './index.js', // エントリポイントのjsxファイル
+  output: {
+    filename: 'bundle.js' // 出力するファイル
+  },
+  devServer: {
+    host: "0.0.0.0",  // useLocalIPを有効化するために必要
+    port: 3000,
+    useLocalIp: true  // URLを`localhost`ではなく`IPアドレス`を指定して表示するのに必要。
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/, // 拡張子がjsで
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader', // babel-loaderを使って変換する
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'], // env presetでES2015向けに変換、react presetでReactのJSX文法を変換
+        }
+      }
+    }]
+  }
+}
